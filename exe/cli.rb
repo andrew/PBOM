2ce4459986a6b4682c2cc5b91ddde20d1a7c8813
Andrew Nesbitt <andrewnez@gmail.com>
2024-06-14 13:44:28 +0100

    Make the input and output paths work

--- exe/cli.rb
+++ exe/cli.rb
@@ -2,6 +2,20 @@
 
 require_relative "../lib/pbom"
 
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: generate [options]"
+
+  opts.on("--input PATH", "Set the input path") do |path|
+    options[:input_path] = path
+  end
+  opts.on("--output PATH", "Set the output path") do |path|
+    options[:output_path] = path
+  end
+end.parse!
+
 # Parse command-line arguments and call the appropriate functions
 # This is just a placeholder. Replace this with your actual CLI code.
 if ARGV.empty?
@@ -12,7 +26,9 @@ else
   when "generate"
     puts "Generating a new PBOM..."
     # TODO input_path and output_path should be parsed from the command line
-    Pbom::Generator.new.generate
+    input_path = options[:input_path] || "."
+    output_path = options[:output_path] || "."
+    Pbom::Generator.new(input_path: input_path, output_path: output_path).generate
   when "version"
     puts "PBOM version #{Pbom::VERSION}"
   else
